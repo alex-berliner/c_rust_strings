3d0cba983973368ec83cbff0a24862a9c4466bee
Alex Berliner <alexberliner@gmail.com>
2023-02-26 12:10:20 -0800

    Example of passing closure to thread

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+struct PointerHolder {
+    ptr: Arc<Mutex<dyn Send + Fn(i32, i32) -> i32>>,
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let add_ptr = add as fn(i32, i32) -> i32;
+    let ph = PointerHolder {
+        ptr: Arc::new(Mutex::new(add_ptr)),
+    };
+    let mut t_handles = vec![];
+    for i in 0..10 {
+        let pc = ph.clone();
+        let handle = std::thread::spawn(move || {
+            let p = pc.ptr;
+            let add_ptr_value = p.lock().unwrap();
+            let result = add_ptr_value(i, i*10);
+            println!("result: {:?}", result);
+        });
+        t_handles.push(handle);
+    }
+
+    for h in t_handles {
+        h.join();
+    }
+}
